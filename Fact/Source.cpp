ac5d1ff4df8951e4ba8c7cabf768cf523ab9233d
Adam Kelly <adam.mark.kelly@gmail.com>
2018-04-08 23:28:49 +0100

    dev/Rev.2 - Vector (Not working)

--- Fact/Source.cpp
+++ Fact/Source.cpp
@@ -32,8 +32,8 @@ void recFact::recFactCalc(int n)
 	//res.at(0) = 1;
 	res.push_back(1);
 	//res[0] = 1;
-	int res_size = sizeof(n);
-	//int res_size = 1; // size of n
+	//int res_size = sizeof(n);
+	int res_size = 1; // size of n
 
 	// Apply simple factorial formula n! = 1 * 2 * 3 * 4...*n
 	for (int x = 2; x <= n; x++)
@@ -51,8 +51,7 @@ int recFact::recFactMult(int x, vector<int> res, int res_size)
 					// One by one multiply n with individual digits of res[]
 	for (int i = 0; i<res_size; i++)
 	{
-		int prod = res[i] * x + carry;
-		int prod = res.push_back(i);
+		int prod = res.at(i) * x + carry;
 		
 
 		// Store last digit of 'prod' in res[]  
@@ -65,7 +64,8 @@ int recFact::recFactMult(int x, vector<int> res, int res_size)
 	// Put carry in res and increase result size
 	while (carry)
 	{
-		res[res_size] = carry % 10;
+		//res.at(res_size) = carry % 10;
+		res.push_back(carry % 10);
 		carry = carry / 10;
 		res_size++;
 	}
